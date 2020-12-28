e4dd33379f9a1c3f64a3b337c57d2b7fb34f66cb
Adam Donatelli <infdonatelli@yahoo.com>
2020-12-27 20:04:24 -0500

    Nav Row on home section complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b7fa0cacd9b4d0a83a9b";
+/******/ 	var hotCurrentHash = "8ba4576d829256e10511";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -11,38 +11,110 @@ export default function Home() {
       </Head>
 
      
+    <div className='firstRowWrapper'>
+
+   
           <div className="homeBanner" 
-          style={{
-            backgroundImage: "url(" + `${require("../public/home-bnr-1200.gif")}` + ")",
+            style={{
+              backgroundImage: "url(" + `${require("../public/home-bnr-1200.gif")}` + ")",
+              width: "100%",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover"
+            }}
+            >
+
+            <div class="bannerContent">
+                <center>
+                  <div className='bannerLogo'>
+                    <Image
+                    src="/../public/Adamedia-Design-Logo.png"
+                    alt="Picture of the author"
+                    width={100}
+                    height={100}
+                    />
+                  </div>
+                  
+                </center>
+              <center> <p>ADAMEDIA DESIGN</p> </center>
+                <center> 
+                  <p>
+                    <span className='whiteSpan'> SOFTWARE </span>  &
+                    <span className='whiteSpan'> MEDIA</span> AT ITS BEST
+                  </p>
+                </center>
+            </div> 
+          </div>
+
+
+
+     
+          <div className="NavRow"
+            style={{
+            backgroundImage: "url(" + `${require("../public/background-b.jpg")}` + ")",
             width: "100%",
             backgroundRepeat: "no-repeat",
             backgroundSize: "cover"
           }}
           >
-          <div class="bannerContent">
-              <center>
-                <div className='bannerLogo'>
-                  <Image
-                  src="/../public/Adamedia-Design-Logo.png"
-                  alt="Picture of the author"
-                  width={100}
-                  height={100}
-                  />
+
+            <div className="hover-box-2">
+                {/* <a href="ux-ui.php">  */}
+                <div className="title">
+                  <h4>SOFTWARE | WEB PROJECTS</h4>
                 </div>
-                
-              </center>
-            <center> <p>ADAMEDIA DESIGN</p> </center>
-              <center> 
-                <p>
-                  <span className='whiteSpan'> SOFTWARE </span>  &
-                  <span className='whiteSpan'> MEDIA</span> AT ITS BEST
-                </p>
-              </center>
-          </div> 
+
+                <div className='image'>
+                <Image
+                      src="/../public/projects-icon.png"
+                      alt="Picture of the author"
+                      width={400}
+                      height={200}
+                      />
+                </div>  
+                {/* </a> */}
+            </div>
+
+
+              <div className="hover-box-2">
+                {/* <a href="about.php">  */}
+                  <div className="title">
+                    <h4>ABOUT</h4>
+                  </div>
+
+                  <div className='image'>
+                  <Image
+                      src="/../public/about-me-icon.png"
+                      alt="Picture of the author"
+                      width={400}
+                      height={250}
+                      />
+                  </div>
+                     
+              </div> 
+                  {/* </a> */}
+            
+              <div className="hover-box-2">
+                {/* <a href="process.php">  */}
+                  <div className="title">
+                      <h4>PROCESS</h4>
+                  </div>
+
+                  <div className='image'>
+                        <Image
+                        src="/../public/process.png"
+                        alt="Picture of the author"
+                        width={300}
+                        height={250}
+                        />
+                  </div>
+                {/* </a> */}
+              </div>
+
+          </div>
+
+
       </div>
-
-
-          <Skills/>
+          {/* <Skills/> */}
       
     </div>
   )

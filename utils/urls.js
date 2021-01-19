df07c3acbe5bf3fff0c0ce56f1578ab2bf81bccd
Adam Donatelli <infdonatelli@yahoo.com>
2021-01-18 21:31:09 -0500

    urls

--- utils/urls.js
+++ utils/urls.js
@@ -1,6 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
 export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || 'pk_test_B91D43CC22E7BB2B';
-export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PK || 'pk_test_51I8XvjD2B2kqWDmPA5xiQSp9CSv4ZrclD7Anq1MMgE3bhIsqF53LWTrKF9FnQvbqgc31bqVPnaN2cvJ6PqUDh4Sf00hOKJUTPM'
+export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PK || 'pk_live_51I8XvjD2B2kqWDmPEvCjYlUXay6BlRqfmL0QsVOG8dr4kZJJIpTHCfIj2G7CcxKIk0m9QCvLSzdPTRribazmR2ar00kMcbSHS0'
 
 /**
  * Given an image return the URL

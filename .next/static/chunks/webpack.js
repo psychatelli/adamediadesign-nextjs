975b15eb64354a9549d91a979ffeced027bf2ce2
Adam Donatelli <infdonatelli@yahoo.com>
2021-10-14 22:46:28 -0400

    overflow scroll for exercise list

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c6af54dcfd63dfce39a4";
+/******/ 	var hotCurrentHash = "02b37fbb17c17634ef5f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
